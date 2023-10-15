73b9ee1f262d0ddd72abc695723088ddb6cfb5a8
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-10-14 23:40:28 -0400

    banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    ///JSX
+    return(
+        <hearder className="banner">
+            <img src="/imgs/banner.png" alt='Banner da pagina'/>
+
+        </hearder>
+
+    );
+}
+export default Banner;
