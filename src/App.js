b0c3b3d12687f0b0036b344c195358376b25658b
Yasmin Ferreira <yasmincferreira8@gmail.com>
2024-01-12 17:37:54 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -17,6 +18,9 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionar(colaborador)}/>
+      <Time nome="Programação"></Time>
+      <Time nome="Front-End"></Time>
+      <Time nome="Data Science"></Time>
       
     </div>
   );
