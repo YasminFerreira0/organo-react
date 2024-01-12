a608f4a52b8851c61cece46d71bc4c4e2f7f42ef
Yasmin Ferreira <yasmincferreira8@gmail.com>
2024-01-12 15:38:24 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionar = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores({...setColaboradores, colaborador})
+
+  }
+
   return (
     
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionar(colaborador)}/>
       
     </div>
   );
